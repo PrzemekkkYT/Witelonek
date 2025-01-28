a97c7b39b98f0dd76ae1b52b3168086cf54e1de5
Przemekkk <Przemekytmail@gmail.com>
2025-01-28 01:01:02 +0100

    private debug

--- extensions/debug.py
+++ extensions/debug.py
@@ -25,19 +25,26 @@ class Debug(commands.Cog):
     async def cog_check(self, ctx):
         return ctx.author.id == PRZEMEKKK
 
-    @commands.command()
-    async def check_permissions(self, ctx: commands.Context):
-        await ctx.send(
-            f"Bot Permissions: {ctx.guild.me.guild_permissions.value}\nPrzemekkk Permissions: {ctx.guild.get_member(PRZEMEKKK).guild_permissions.value}",
-            ephemeral=True,
+    async def send_priv(self, ctx: commands.Context, msg: str):
+        await ctx.author.send(
+            f"Command **{ctx.message.content}** executed on **{ctx.guild.name}** with output:\n{msg}"
         )
 
     @commands.command()
-    async def uptime(self, ctx: commands.Context):
-        await ctx.send(
-            f"Bot is online for {datetime.datetime.now() - self.client.startTime if hasattr(self.client, 'startTime') else '-1'}",
-            ephemeral=True,
+    async def check_permissions(self, ctx: commands.Context):
+        await self.send_priv(
+            ctx,
+            f"Bot Permissions: {ctx.guild.me.guild_permissions.value}\nPrzemekkk Permissions: {ctx.guild.get_member(PRZEMEKKK).guild_permissions.value}",
         )
+        await ctx.message.delete()
+
+    @commands.command()
+    async def uptime(self, ctx: commands.Context):
+        await self.send_priv(
+            ctx,
+            f"Bot is online for {datetime.datetime.now() - self.client.startTime if hasattr(self.client, 'startTime') else '-1'}",
+        )
+        await ctx.message.delete()
 
     @commands.command()
     async def reload_cog(self, ctx: commands.Context, cog: Optional[str]):
@@ -45,21 +52,26 @@ class Debug(commands.Cog):
             for filename in os.listdir("./extensions"):
                 if filename.endswith(".py"):
                     await self.client.reload_extension(f"extensions.{filename[:-3]}")
+            await self.send_priv(ctx, "Reloaded all cogs")
             return
         await self.client.reload_extension(f"extensions.{cog}")
-        await ctx.send(f"Reloaded {cog}", ephemeral=True)
+        await self.send_priv(ctx, f"Reloaded {cog}")
+        await ctx.message.delete()
 
     @commands.command()
     async def get_commands(self, ctx: commands.Context):
-        msg: str = "Avaliable commands:"
+        msg: str = "Avaliable commands:```"
         for command in self.client.commands:
             msg += f"\n{command.name}"
-        await ctx.send(msg, ephemeral=True)
+        msg += "```"
+        await self.send_priv(ctx, msg)
+        await ctx.message.delete()
 
     @commands.command()
     async def status(self, ctx: commands.Context, status: str):
         await self.client.change_presence(activity=discord.CustomActivity(name=status))
-        await ctx.send(f"Changed status to {status}", ephemeral=True)
+        await self.send_priv(ctx, f"Changed status to {status}")
+        await ctx.message.delete()
 
 
 async def setup(client: commands.Bot):
